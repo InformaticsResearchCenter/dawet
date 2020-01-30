827c6afbb81dfdc4d8936c6c4cab15ed017b7d2a
trianggadios <trianggadio@gmail.com>
2020-01-30 11:06:18 +0700

    setRowData

--- dawet.py
+++ dawet.py
@@ -13,15 +13,28 @@ class Dawet(object):
         client = gspread.authorize(creds)
         self.sheet = client.open(self.filename)
 
+    def getAllData(self, sheetnum):
+        list_value = self.sheet.get_worksheet(sheetnum).get_all_values()
+
+        return list_value
+
     def getData(self, rowname, colname, sheetnum):
         dataError = True
         while dataError:
             try:
                 ambil = self.sheet.get_worksheet(sheetnum).cell(self.sheet.get_worksheet(sheetnum).find(rowname).row, self.sheet.get_worksheet(sheetnum).find(colname).col).value
                 print(colname + " selesai")
+                dataError = False
                 return ambil
             except Exception as e:
-                if str(e).find("RESOURCE_EXHAUSTED"):
+                print(e)
+                if str(e) == rowname:
+                    dataError = False
+                    return "not_found"
+                elif str(e) == colname:
+                    dataError = False
+                    return "pertemuan_not_found"
+                else:
                     print("wait ...")
                     time.sleep(10)
                     dataError = True
@@ -29,3 +42,13 @@ class Dawet(object):
     def setData(self, rowname, colname, sheetnum, content):
         setv = self.sheet.get_worksheet(sheetnum).update_cell(self.sheet.get_worksheet(sheetnum).find(rowname).row, self.sheet.get_worksheet(sheetnum).find(colname).col, content)
         return setv
+
+    def setRowData(self, data):
+        dataLimit = True
+        while dataLimit:
+            try:
+                self.sheet.get_worksheet(0).insert_row(data, 2)
+                dataLimit = False
+            except:
+                time.sleep(10)
+                dataLimit = True
